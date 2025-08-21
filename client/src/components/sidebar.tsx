c806986906d679d23e8a4562995f1a26adc498d1
Renz Cole <renz2arjhay@gmail.com>
2025-08-21 13:26:28 +0800

    added image

--- client/src/components/sidebar.tsx
+++ client/src/components/sidebar.tsx
@@ -3,8 +3,10 @@ import React from "react";
 
 const Sidebar: React.FC = () => {
   return (
-    <div className="shadow-lg">
-      <div className="w-full h-30 []"></div>
+    <div className="shadow-lg bg-[#fafafa]">
+      <div className="w-full h-30 [background-image:linear-gradient(rgba(0,0,0,0.2),rgba(0,0,0,0.65)),url('/img/cover.jpg')] bg-center bg-cover">
+        <div></div>
+      </div>
       <nav className="list-none">
         <li className="px-4 py-2 flex items-center gap-4 hover:text-white hover:bg-primary">
           <Home size={20} /> Dashboard
