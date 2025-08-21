f62c973d6487531d08305f6dfc69b1b75f17810c
Renz Cole <renz2arjhay@gmail.com>
2025-08-21 19:09:06 +0800

    added react-query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Layout from "./layout";
 import Dashboard from "./pages/dashboard";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/login";
+import ManageAccounts from "./pages/accounts";
 
 const App: React.FC = () => {
   return (
     <Layout>
-      <Dashboard />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/students" element={<ManageAccounts />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </Layout>
   );
 };
