cd4f2633661e65ba0d02659e1a50846f8d605d78
Renz Cole <renz2arjhay@gmail.com>
2025-08-21 13:26:50 +0800

    update ui

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -1,7 +1,28 @@
+import { Users } from "lucide-react";
 import React from "react";
 
 const Dashboard: React.FC = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="p-5">
+      <h1 className="text-xl px-1.5 border-l-4 leading-6 border-primary font-bold mb-4">
+        Dashboard
+      </h1>
+
+      <div className="flex justify-between p-4 rounded-lg bg-linear-to-r from-teal-500 to-teal-300 relative shadow">
+        <div>
+          <h2 className="text-2xl text-white font-bold">Total Students</h2>
+          <p className="text-sm text-gray-100">Dormitory InfoSys</p>
+        </div>
+
+        <Users
+          className="text-[rgba(0,0,0,0.2)] absolute right-3 top-1/2 transform -translate-y-1/2"
+          size={60}
+        />
+
+        <div className="text-4xl pr-10 text-white font-bold mt-2 z-10">150</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
