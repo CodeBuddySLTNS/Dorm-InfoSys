2a607eda9f3444febc312e082c90215ef6c9c227
Renz Cole <renz2arjhay@gmail.com>
2025-08-21 15:23:26 +0800

    update sidebar

--- client/src/components/sidebar.tsx
+++ client/src/components/sidebar.tsx
@@ -4,8 +4,14 @@ import React from "react";
 const Sidebar: React.FC = () => {
   return (
     <div className="shadow-lg bg-[#fafafa]">
-      <div className="w-full h-30 [background-image:linear-gradient(rgba(0,0,0,0.2),rgba(0,0,0,0.65)),url('/img/cover.jpg')] bg-center bg-cover">
-        <div></div>
+      <div className="w-full h-30 p-3 [background-image:linear-gradient(rgba(0,0,0,0.2),rgba(0,0,0,0.65)),url('/img/cover.jpg')] bg-center bg-cover">
+        <div>
+          <img
+            className="w-16 h-16 rounded-full shadow border-2 border-primary"
+            src="/img/logo.png"
+          />
+          <h3 className="text-white text-xl">Administrator</h3>
+        </div>
       </div>
       <nav className="list-none">
         <li className="px-4 py-2 flex items-center gap-4 hover:text-white hover:bg-primary">
