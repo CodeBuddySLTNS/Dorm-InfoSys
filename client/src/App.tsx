a4b163b767f7f5876cb1ce573952dfc9c102a98d
Renz Cole <renz2arjhay@gmail.com>
2025-08-22 15:11:15 +0800

    refactor App and Header components: enhance session management and improve logout functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "./layout";
 import Dashboard from "./pages/dashboard";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import Login from "./pages/login";
 import ManageAccounts from "./pages/accounts";
 import Signup from "./pages/signup";
+import { useMainStore } from "./store";
+import { useQuery } from "@tanstack/react-query";
+import { coleAPI } from "./lib/utils";
+import { Loader } from "lucide-react";
 
 const App: React.FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const user = useMainStore((state) => state.user);
+  const { data, isLoading } = useQuery({
+    queryKey: ["session"],
+    queryFn: coleAPI("/auth/session"),
+  });
+
+  useEffect(() => {
+    if (data) {
+      useMainStore.getState().setLoggedIn(true);
+      useMainStore.getState().setUser(data);
+    }
+
+    if (!isLoading && !data && !useMainStore.getState().user) {
+      navigate("/login");
+    }
+  }, [data, isLoading]);
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/login":
+        if (user) navigate("/");
+    }
+  }, [location.pathname, user, navigate]);
+
+  if (isLoading)
+    return (
+      <div className="w-full h-dvh flex items-center justify-center">
+        <Loader size={60} className="animate-spin text-primary" />
+      </div>
+    );
+
   return (
     <Layout>
       <Routes>
--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,20 +1,39 @@
-import { Menu } from "lucide-react";
+import { useMainStore } from "@/store";
+import { LogOut, Menu } from "lucide-react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    useMainStore.getState().setLoggedIn(false);
+    useMainStore.getState().setUser(null);
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
   return (
-    <div className="flex items-center bg-primary shadow-lg">
-      <div className="p-3 bg-[#42bccc]">
-        <Menu className="text-white" />
-      </div>
-      <div className="flex items-center gap-1.5 p-1 px-2">
-        <img className="w-[35px]" src="/img/logo.png" alt="logo" />
-        <div className="text-white">
-          <h1 className="leading-4.5 uppercase font-bold">
-            Philippine Advent College Inc.
-          </h1>
-          <p className="leading-4.5">Dormitory Information System</p>
+    <div className="flex justify-between items-center bg-primary shadow-lg">
+      <div className="flex items-center">
+        <div className="p-3 bg-[#42bccc]">
+          <Menu className="text-white" />
         </div>
+        <div className="flex items-center gap-1.5 p-1 px-2">
+          <img className="w-[35px]" src="/img/logo.png" alt="logo" />
+          <div className="text-white">
+            <h1 className="leading-4.5 uppercase font-bold">
+              Philippine Advent College Inc.
+            </h1>
+            <p className="leading-4.5">Dormitory Information System</p>
+          </div>
+        </div>
+      </div>
+      <div
+        className="text-sm flex items-center gap-1 p-1 mr-3 rounded bg-red-700 hover:bg-red-600 text-white cursor-pointer transition-colors duration-200"
+        onClick={handleLogout}
+      >
+        <LogOut size={18} />
+        Logout
       </div>
     </div>
   );
