2b6aeb3cffc6b7d331947601d4a5bd70abfddb31
Renz Cole <renz2arjhay@gmail.com>
2025-08-22 15:10:23 +0800

    add Toaster component using sonner and integrate it into main application

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,13 +4,21 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
+        <Toaster position="top-right" richColors />
         <App />
       </QueryClientProvider>
     </BrowserRouter>
